91fe6d7cbbcca7ac5648d22de3a7b87fd4914054
shavkat2610 <waldapotheker95@gmail.com>
2025-11-13 18:05:46 +0100

    m

--- dev.py
+++ dev.py
@@ -14,12 +14,14 @@ from scripts.helper_methods import imagesearch, reset_client_window
 def screenshot_area(point = (0, 0)):
     time.sleep(2.5)
     reset_client_window()
-    im = pyautogui.screenshot(region=(point[0], point[1], 50, 50))
+    time.sleep(3.5)
+    time.sleep(3.5)
+    im = pyautogui.screenshot(region=(point[0], point[1], 250, 250))
     # secs = time.time()
     # im2 = pyautogui.screenshot(region=(8, 32, 50, 50))
     im.save('temp.png')
 
-screenshot_area(point=(1177, 377))
+screenshot_area(point=(420, 170))
 
 
 

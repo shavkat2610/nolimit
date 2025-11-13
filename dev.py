8cf9d6037ff8f2c4956546562678be142497a787
shavkat2610 <waldapotheker95@gmail.com>
2025-11-13 20:45:44 +0100

    mmmmmmmmm

--- dev.py
+++ dev.py
@@ -16,12 +16,12 @@ def screenshot_area(point = (0, 0)):
     reset_client_window(debug=False)
     time.sleep(3.5)
     time.sleep(3.5)
-    im = pyautogui.screenshot(region=(point[0], point[1], 500, 500))
+    im = pyautogui.screenshot(region=(point[0], point[1], 50, 50))
     # secs = time.time()
     # im2 = pyautogui.screenshot(region=(8, 32, 50, 50))
     im.save('holdem.png')
 
-screenshot_area(point=(350, 172))
+screenshot_area(point=(350, 142))
 
 
 

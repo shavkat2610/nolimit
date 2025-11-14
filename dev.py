df9814d80d0e7dfd47b6c5589dbd7c01891d5288
shavkat95 <shavkat95@icloud.com>
2025-11-14 21:38:28 +0100

    123

--- dev.py
+++ dev.py
@@ -16,12 +16,12 @@ def screenshot_area(point = (0, 0)):
     reset_client_window(debug=False)
     time.sleep(3.5)
     time.sleep(3.5)
-    im = pyautogui.screenshot(region=(point[0], point[1], 100, 25))
+    im = pyautogui.screenshot(region=(point[0], point[1], 50, 50))
     # secs = time.time()
     # im2 = pyautogui.screenshot(region=(8, 32, 50, 50))
-    im.save('temp.png')
+    im.save('dark_icon.png')
 
-screenshot_area(point=(740, 40))
+screenshot_area(point=(7, 45))
 
 
 

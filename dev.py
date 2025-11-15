ec687be0e9a48b4084d6f0f55b8f050e0e2c1a98
shavkat95 <shavkat95@icloud.com>
2025-11-15 10:39:14 +0100

    m

--- dev.py
+++ dev.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 # from matplotlib import pyplot as plt
 
-from scripts.helper_methods import imagesearch, reset_client_window
+from scripts.helper_methods import imagesearch, reset_client_window, def_clint
 
 
 
@@ -14,12 +14,12 @@ from scripts.helper_methods import imagesearch, reset_client_window
 def screenshot_area(point = (0, 0)):
     time.sleep(6.5)
     reset_client_window(debug=False)
-    im = pyautogui.screenshot(region=(point[0], point[1], 250, 100))
+    im = pyautogui.screenshot(region=(point[0], point[1], 1100, 300))
     # secs = time.time()
     # im2 = pyautogui.screenshot(region=(8, 32, 50, 50))
-    im.save('dark_icon_3.png')
+    im.save('temp31.png')
 
-screenshot_area(point=(420, 145))
+screenshot_area(point=def_clint)
 
 
 

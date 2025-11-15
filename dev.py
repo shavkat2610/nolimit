7c40bba469f9c595479471bd82f06e5fdeb738ae
shavkat95 <shavkat95@icloud.com>
2025-11-15 10:47:01 +0100

    m

--- dev.py
+++ dev.py
@@ -13,7 +13,7 @@ from scripts.helper_methods import imagesearch, reset_client_window, def_clint
 
 def screenshot_area(point = (0, 0)):
     time.sleep(6.5)
-    reset_client_window(debug=False)
+    # reset_client_window(debug=False)
     im = pyautogui.screenshot(region=(point[0], point[1], 1100, 300))
     # secs = time.time()
     # im2 = pyautogui.screenshot(region=(8, 32, 50, 50))

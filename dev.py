d9892a8d9be44ef6474eaad22a96ceb2b4793ba1
shavkat2610 <waldapotheker95@gmail.com>
2025-11-15 10:23:17 +0100

    mmm

--- dev.py
+++ dev.py
@@ -14,12 +14,12 @@ from scripts.helper_methods import imagesearch, reset_client_window
 def screenshot_area(point = (0, 0)):
     time.sleep(6.5)
     reset_client_window(debug=False)
-    im = pyautogui.screenshot(region=(point[0], point[1], 550, 550))
+    im = pyautogui.screenshot(region=(point[0], point[1], 250, 100))
     # secs = time.time()
     # im2 = pyautogui.screenshot(region=(8, 32, 50, 50))
     im.save('dark_icon_3.png')
 
-screenshot_area(point=(400, 145))
+screenshot_area(point=(420, 145))
 
 
 

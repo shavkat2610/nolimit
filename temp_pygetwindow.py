605b41dc6763b6471fd05d407836a25d9e7b973b
shavkat2610 <waldapotheker95@gmail.com>
2025-11-19 12:11:00 +0100

    dvf g sg.

--- temp_pygetwindow.py
+++ temp_pygetwindow.py
@@ -38,6 +38,8 @@ time.sleep(3)
 # also able to edit z3 to specified window-title string like: "Sublime Text (UNREGISTERED)"
 # my = pygetwindow.getWindowsWithTitle(z3)[0]
 my = pygetwindow.getActiveWindow()
+print(my)
+exit()
 # quarter of screen screensize
 x3 = x2 // 2
 y3 = y2 // 2
